8bf2f8d9c1b0fa5732dfc7205ec625c8f732bddf
Yohan Bradley <yohanbradley24@gmail.com>
2026-01-10 00:26:23 +0100

    Day 2 commit

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,6 +2,9 @@ export default function NavigationBar() {
   return (
     <nav className="navbar">
       <h1>GettyLand</h1>
+      <p>Coding day 2  SRS made</p>
+      <p>Heard from Renewed mind about routing though concept still to be studied</p>
+      <p>It's 00:29 got to push this code and sleep see you tomorrow King</p>
       
     </nav>
   )
