5137d64130c351a7e90a66d8a77c4d81296ba9aa
Yohan Bradley <yohanbradley24@gmail.com>
2026-01-10 21:29:03 +0100

    fourth commit

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,7 +3,7 @@ export default function NavigationBar() {
     <nav className="navbar">
       <h1>GettyLand</h1>
       <p>Coding day 3</p>
-      <p>I showed SRS to Gabby , i'm asking myself if the project name is really professional and whether she liked my lenses or not</p>
+      <p>I'm asking myself if the project name is really professional</p>
       <p>Gush i've got to start coding!!!</p>
       
     </nav>
